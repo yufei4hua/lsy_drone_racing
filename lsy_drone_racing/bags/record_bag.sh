ba02b58c4f7cb3201f4103208db623d123639258
Jiaming Zhang <jiaming.zhang@tum.de>
2025-06-07 06:15:46 +0200

    Create record_bag.sh

---
+++ lsy_drone_racing/bags/record_bag.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+eval "$(micromamba shell hook --shell=bash)"
+micromamba activate race
+
+BAG_NAME=${1:-my_bag}
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+FULL_NAME="${BAG_NAME}_${TIMESTAMP}"
+
+shift
+TOPICS="$@"
+
+if [ -z "$TOPICS" ]; then
+  echo "No topics specified!"
+  exit 1
+fi
+
+echo "Recording to: $FULL_NAME"
+echo "Topics: $TOPICS"
+ros2 bag record -o "$FULL_NAME" $TOPICS
