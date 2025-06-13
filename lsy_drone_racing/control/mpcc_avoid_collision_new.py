29e869d87abffc5626e077ca54896ace55541a48
yufei4hua <michael4hua@gmail.com>
2025-06-13 12:16:23 +0200

    chore: clear up unnecessary import

--- lsy_drone_racing/control/mpcc_avoid_collision_new.py
+++ lsy_drone_racing/control/mpcc_avoid_collision_new.py
@@ -9,27 +9,18 @@ Note that the trajectory uses pre-defined waypoints instead of dynamically gener
 
 from __future__ import annotations  # Python 3.10 type hints
 
-from statistics import pvariance
 from typing import TYPE_CHECKING, List, Dict, Callable, Optional, Union, Set
 import os
-from Cython import p_void
-from matplotlib.offsetbox import VPacker
 import numpy as np
-import scipy
 from acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver
-from casadi import MX, DM, cos, sin, vertcat, dot, norm_2, floor, if_else, exp, Function, power, fmin
-from scipy import linalg
-from scipy.fft import prev_fast_len
+from casadi import MX, DM, cos, sin, vertcat, dot, norm_2, floor, if_else, exp, Function, power
 from scipy.interpolate import CubicSpline
 from scipy.spatial.transform import Rotation as R
 from casadi import interpolant
-from sympy import true
-from traitlets import TraitError
 
 from lsy_drone_racing.control.fresssack_controller import FresssackController
 from lsy_drone_racing.control.fresssack_controller import MultiArrayTx, MarkerArrayTx, CapsuleMarkerTx, MeshMarkerTx, PathTx, TFTx
 
-from lsy_drone_racing.control.easy_controller import EasyController
 from lsy_drone_racing.control import Controller
 from lsy_drone_racing.tools.ext_tools import TrajectoryTool, TransformTool
 from lsy_drone_racing.utils.utils import draw_line
